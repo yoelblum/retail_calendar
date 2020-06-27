168eff57b8a139e68f3f761c5c3ad6211920a0c5
Joel Blum <j.blum@youngcapital.nl>
2020-06-27 21:45:56 +0200

    initial commit

---
+++ lib/retail_calendar.rb
@@ -0,0 +1,11 @@
+require "retail_calendar/version"
+require "retail_calendar/calendar"
+require "date"
+require 'active_support'
+require 'active_support/core_ext'
+# Class for 5-4-4, 4-5-4, and 4-4-5 calendars
+# https://stackoverflow.com/questions/23531481/is-there-a-way-in-ruby-to-find-the-beginning-of-the-year-in-a-4-5-4-retail-cal
+module RetailCalendar
+end
+
+
---
+++ lib/retail_calendar/version.rb
@@ -0,0 +1,3 @@
+module RetailCalendar
+  VERSION = "0.1.0"
+end
